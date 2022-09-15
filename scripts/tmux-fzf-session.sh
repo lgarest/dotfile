bdcf45a0715a8bc31b1d83a1fba29a83c552798a
Luis Garcia <luis.garciaestrades@gartner.com>
2022-09-15 14:17:06 +0200

    Make tmux-fzf-session command ignore current tmux session in list results

--- scripts/tmux-fzf-session.sh
+++ scripts/tmux-fzf-session.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+current=`tmux display-message -p '#S'`
 # get active sessions, fzf them and get only the name
-session=$(tmux list-session -F "#{session_name}" | fzf)
+session=$(tmux list-session -F "#{session_name}" | grep -v "$current" | fzf)
 
 # switch to the selection
 tmux switch-client -t "$session"
