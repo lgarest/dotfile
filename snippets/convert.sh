1082ba60c4797379f108fbd16704f3918acfe99d
Luis Garcia <luis.garciaestrades@gartner.com>
2023-07-05 09:07:17 +0200

    Update snippets

---
+++ snippets/convert.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+npm i -g convert-snippets
+
+# converts to ultisnippets format to json format
+
+cat testing.snippets react.snippets javascript.snippets  > _javascript.snippets \
+  && convert-snippets _javascript.snippets javascript.json \
+  && rm _javascript.snippets
