1905769660ae3da3d9a32bc51be1beeeb82decfa
Luis Garcia <luis.garciaestrades@gartner.com>
2023-07-07 12:12:58 +0200

    Format overrides.lua

--- nvim-lua/custom/configs/overrides.lua
+++ nvim-lua/custom/configs/overrides.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+
 M.treesitter = {
 	ensure_installed = {
 		"vim",
@@ -48,18 +49,18 @@ M.mason = {
 
 -- git support in nvimtree
 M.nvimtree = {
-  git = {
-    enable = true,
-  },
+	git = {
+		enable = true,
+	},
 
-  renderer = {
-    highlight_git = true,
-    icons = {
-      show = {
-        git = true,
-      },
-    },
-  },
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
 }
 
 return M
