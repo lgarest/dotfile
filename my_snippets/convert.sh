1752ed181894f1c4cb8d22f175720753e0863184
Luis Garcia <luis.garciaestrades@gartner.com>
2023-10-10 22:28:53 +0200

    Move snippets

--- my_snippets/convert.sh
+++ my_snippets/convert.sh
@@ -5,5 +5,5 @@ npm i -g convert-snippets
 # converts to ultisnippets format to json format
 
 cat testing.snippets react.snippets javascript.snippets  > _javascript.snippets \
-  && convert-snippets _javascript.snippets javascript.json \
+  && convert-snippets _javascript.snippets typescript.json \
   && rm _javascript.snippets
