5062ed8611006416d61e4989d563bac74027ecb6
Luis Garcia Estrades <luis.garciaestrades@gartner.com>
2024-07-29 09:48:13 +0200

    Use conform for formatters

--- nvim-lua2/configs/conform.lua
+++ nvim-lua2/configs/conform.lua
@@ -1,15 +1,17 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		typescript = { "prettierd" },
+		markdown = { "markdownlint" },
+	},
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 require("conform").setup(options)
