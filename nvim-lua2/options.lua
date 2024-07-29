7320c831654ec5a178413266dc1f697e80e109ee
Luis Garcia Estrades <luis.garciaestrades@gartner.com>
2024-07-29 09:47:27 +0200

    Add prototype for options with k,v

--- nvim-lua2/options.lua
+++ nvim-lua2/options.lua
@@ -1,9 +1,8 @@
-require "nvchad.options"
+require("nvchad.options")
 
 -- add yours here!
 
 -- local o = vim.o
--- o.cursorlineopt ='both' -- to enable cursorline!
 -- local autocmd = vim.api.nvim_create_autocmd
 
 -- Auto resize panes when resizing nvim window
@@ -11,7 +10,7 @@ require "nvchad.options"
 --   pattern = "*",
 --   command = "tabdo wincmd =",
 -- })
-local autocmd = vim.api.nvim_create_autocmd
+-- local autocmd = vim.api.nvim_create_autocmd
 -- autocmd("VimEnter", {
 --   pattern = "*",
 --   command = "NvimTreeToggle"
@@ -22,33 +21,50 @@ local autocmd = vim.api.nvim_create_autocmd
 -- })
 
 -- Changes for Nvim
+vim.opt.cursorlineopt = "both" -- to enable cursorline!
 vim.opt.colorcolumn = { "80", "100" }
 vim.wo.relativenumber = true
 -- incremental searches
 vim.opt.incsearch = true
-
 vim.opt.scrolloff = 8
-
 -- mousescroll=ver:5,hor:2
 vim.opt.mousescroll = { "ver:1", "hor:2" }
 vim.opt.isfname:append("@-@")
-
 vim.filetype.add({
 	pattern = {
 		[".*conf.template"] = "nginx",
 	},
 })
-
 vim.opt.winbar = "%=%m %f"
-
 vim.opt.swapfile = false
 vim.opt.switchbuf = "vsplit"
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- @TODO: change for this
+-- local optionss = {
+--   swapfile = false, -- no swapfile
+--   switchbuf = "vsplit", -- switch to vertical split
+--   splitbelow = true, -- split below
+--   splitright = true, -- split right
+--   winbar = "%=%m %f", -- show modified and filename
+--   mousescroll = "ver:1,hor:2", -- mouse scroll
+--   scrolloff = 8, -- lines of context
+--   incsearch = true, -- incremental searches
+--   colorcolumn = "80,100", -- colorcolumn at 80 and 100
+--   relativenumber = true, -- relative line numbers
+--   isfname = vim.o.isfname .. "@-@", -- allow @ in filenames
+--   cursorlineopt = "both", -- enable cursorline
+-- }
+--
+-- for k, v in pairs(optionss) do
+--   vim.opt[k] = v
+-- end
 
 -- vim.g.snipmate_snippets_path = "~/dev/dotfile/snippets/"
 vim.g.vscode_snippets_path = vim.fn.stdpath("config") .. "/lua/custom/my_snippets"
 -- vim.g.snipmate_snippets_path = "~/dev/dotfile/my_snippets"
 --
-
 
 -- @TODO clean this
 local function open_nvim_tree(data)
@@ -85,16 +101,16 @@ set_indentation(2)
 -- move to setup fn for copilot
 -- require("configs.copilot")
 
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 vim.g.have_nerd_font = true
 
-vim.cmd [[
+vim.cmd([[
     cnoreabbrev W! w!
     cnoreabbrev W1 w!
     cnoreabbrev w1 w!
@@ -112,9 +128,12 @@ vim.cmd [[
     cnoreabbrev Qa qa
     cnoreabbrev QA qa
     cnoreabbrev qA qa
+    cnoreabbrev XA xa
+    cnoreabbrev Xa xa
     cnoreabbrev Vs vs
     cnoreabbrev VS vs
     cnoreabbrev vS vs
     cnoreabbrev Ack Ack!
+    cnoreabbrev LAzy Lazy
     command! LuaSnipEdit :lua require("luasnip.loaders").edit_snippet_files()
-]]
+]])
