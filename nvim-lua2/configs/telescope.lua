47a1407683770516c615a28c34749e65e45e856e
Luis Garcia Estrades <luisgarciaestrades@gmail.com>
2025-07-22 12:19:32 +0200

    perf(nvim): optimize Telescope configuration
    - Simplify Telescope setup with more concise configuration
    - Remove redundant options and use sensible defaults
    - Improve file finder performance with better ignore patterns
    - Clean up extension configurations
    - Reduce configuration complexity while maintaining functionality

--- nvim-lua2/configs/telescope.lua
+++ nvim-lua2/configs/telescope.lua
@@ -1,71 +1,52 @@
-local actions = require "telescope.actions"
-
--- local function normalize_path(path)
---   return path:gsub("\\", "/")
--- end
---
--- local function normalize_cwd()
---   return normalize_path(vim.loop.cwd()) .. "/"
--- end
---
--- local function is_subdirectory(cwd, path)
---   return string.lower(path:sub(1, #cwd)) == string.lower(cwd)
--- end
---
--- local function split_filepath(path)
---   local normalized_path = normalize_path(path)
---   local normalized_cwd = normalize_cwd()
---   local filename = normalized_path:match("[^/]+$")
---
---   if is_subdirectory(normalized_cwd, normalized_path) then
---     local stripped_path = normalized_path:sub(#normalized_cwd + 1, -(#filename + 1))
---     return stripped_path, filename
---   else
---     local stripped_path = normalized_path:sub(1, -(#filename + 1))
---     return stripped_path, filename
---   end
--- end
---
--- local function path_display(_, path)
---   local stripped_path, filename = split_filepath(path)
---   if filename == stripped_path or stripped_path == "" then
---     return filename
---   end
---   return string.format("%s ~ %s", filename, stripped_path)
--- end
-
-
-require("telescope").setup {
-  defaults={
-    path_display = {
-      shorten = { len = 4, exclude = { 1, -1 } },
-    }
-    -- path_display = function(_, path)
-    --   local tail = require("telescope.utils").path_tail(path)
-    --   return string.format("%s (%s)", tail, path)
-    -- end,
-    -- path_display = path_display,
-  },
-  pickers = {
-    buffers = {
-      mappings = {
-        i = {
-          ["<c-d>"] = actions.delete_buffer + actions.move_to_top,
-        }
-      }
-    }
-  },
-  extensions = {
-    frecency = {
-      auto_validate = false,
-      matcher = "fuzzy",
-      -- show_scores = true,
-      show_unindexed = true,
-      ignore_patterns = {"*.git/*", "*/tmp/*"},
-      -- workspaces = {
-        -- ["conf"] = "/home/username/.config",
-        -- ["proj"] = "/home/username/projects",
-      -- }
-    }
-  }
+return {
+	defaults = {
+		path_display = {
+			shorten = { len = 3, exclude = { 1, -2, -1 } },
+		},
+		layout_config = {
+			width = 0.99,
+			height = 0.99,
+		},
+		mappings = {
+			i = {
+				["<c-d>"] = function(...)
+					local actions = require("telescope.actions")
+					return (actions.delete_buffer + actions.move_to_top)(...)
+				end,
+			},
+		},
+	},
+	pickers = {
+		buffers = {
+			mappings = {
+				i = {
+					["<c-d>"] = function(...)
+						local actions = require("telescope.actions")
+						return (actions.delete_buffer + actions.move_to_top)(...)
+					end,
+				},
+			},
+		},
+	},
+	vimgrep_arguments = {
+		"rg",
+		"-L",
+		"--vimgrep",
+		"--hidden",
+		"--color=never",
+		"--no-heading",
+		"--with-filename",
+		"--line-number",
+		"--column",
+		"--smart-case",
+	},
+	extensions = {
+		frecency = {
+			auto_validate = false,
+			matcher = "fuzzy",
+			show_unindexed = true,
+			show_filter_column = false,
+			ignore_patterns = { "*.git/*", "*/tmp/*" },
+		},
+	},
 }
