45cc30658d89cc62671ea52bd167618c41a20889
Luis Garcia Estrades <luisgarciaestrades@gmail.com>
2025-10-16 14:11:45 +0200

    Extend default nvchad lsp settings

--- nvim-lua2/configs/lspconfig.lua
+++ nvim-lua2/configs/lspconfig.lua
@@ -1,15 +1,15 @@
 -- EXAMPLE
 -- local on_attach = require("nvchad.configs.lspconfig").on_attach
 -- local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
--- local nvlsp = require("nvchad.configs.lspconfig")
+-- local capabilities = require("nvchad.configs.lspconfig").capabilities
+local nvlsp = require("nvchad.configs.lspconfig")
 
 -- local lspconfig = vim.lsp.config
 local servers = { "html", "cssls", "ts_ls", "biome", "tailwindcss" }
 
 -- Force all clients to use UTF-8 encoding
-capabilities.offsetEncoding = {'utf-8'}
-capabilities.positionEncodings = {'utf-8'}
+nvlsp.capabilities.offsetEncoding = {'utf-8'}
+nvlsp.capabilities.positionEncodings = {'utf-8'}
 
 -- lsps with default config
 vim.lsp.enable(servers)
@@ -21,6 +21,13 @@ vim.lsp.enable(servers)
 --      capabilities = capabilities
 --    })
 -- end
+
+-- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvchad/configs/lspconfig.lua#L83
+vim.lsp.config('*', { 
+  capabilities = nvlsp.capabilities,
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+})
 
 
 
